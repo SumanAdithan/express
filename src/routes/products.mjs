7f67a69655c8109ee337b6b27e061ea3319a11b5
SumanAdithan <sumanadithang34@gmail.com>
2024-09-09 22:06:31 +0530

    cookies

--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,7 +3,12 @@ const router = Router();
 
 // get request - localhost:3000/api/products
 router.get('/api/products', (req, res) => {
-    res.send([{ id: 123, name: 'chicken rice', price: 12.99 }]);
+    console.log(req.headers.cookie);
+    console.log(req.cookies);
+    console.log(req.signedCookies.hello);
+    if (req.signedCookies.hello && req.signedCookies.hello === 'world')
+        return res.send([{ id: 123, name: 'chicken rice', price: 12.99 }]);
+    return res.status(403).send({ msg: 'Sorry. you need the correct cookie' });
 });
 
 export default router;
