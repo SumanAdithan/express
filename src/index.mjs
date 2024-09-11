861e20c5347b27fb8ac00fa3fe32d9dc36ae4df9
SumanAdithan <sumanadithang34@gmail.com>
2024-09-11 22:19:06 +0530

    session auth

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@ import express, { request, response } from 'express';
 import routes from './routes/index.mjs';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
+import { mockUsers } from './utils/constants.mjs';
 
 const app = express();
 
@@ -27,6 +28,47 @@ app.get('/', (req, res) => {
     req.session.visited = true;
     res.cookie('hello', 'world', { maxAge: 30000, signed: true });
     res.status(201).send({ msg: 'hello' });
+});
+
+app.post('/api/auth', (req, res) => {
+    const {
+        body: { username, password },
+    } = req;
+    console.log(username);
+    const findUser = mockUsers.find(user => user.username === username);
+    if (!findUser || findUser.password !== password)
+        return res.status(401).send({ msg: 'BAD CREDENTIALS' });
+
+    req.session.user = findUser;
+    return res.status(200).send(findUser);
+});
+
+app.get('/api/auth/status', (req, res) => {
+    req.sessionStore.get(req.sessionID, (err, session) => {
+        console.log(session);
+    });
+    return req.session.user
+        ? res.status(200).send(req.session.user)
+        : res.status(401).send({ msg: 'Not Authenticated' });
+});
+
+app.post('/api/cart', (req, res) => {
+    if (!req.session.user) return res.sendStatus(401);
+    const { body: item } = req;
+
+    const { cart } = req.session;
+
+    if (cart) {
+        cart.push(item);
+    } else {
+        req.session.cart = [item];
+    }
+    return res.status(201).send(item);
+});
+
+app.get('/api/cart', (req, res) => {
+    if (!req.session.user) return res.sendStatus(401);
+    return res.send(req.session.cart ?? []);
 });
 
 app.listen(PORT, () => console.log(`Running on Port ${PORT}`));
--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,9 +1,9 @@
 export const mockUsers = [
-    { id: 1, username: 'john', displayName: 'John' },
-    { id: 2, username: 'wick', displayName: 'Wick' },
-    { id: 3, username: 'hulk', displayName: 'Hulk' },
-    { id: 4, username: 'spiderman', displayName: 'Spiderman' },
-    { id: 5, username: 'thor', displayName: 'Thor' },
-    { id: 6, username: 'batman', displayName: 'Batman' },
-    { id: 7, username: 'superman', displayName: 'Superman' },
+    { id: 1, username: 'john', displayName: 'John', password: 'hello123' },
+    { id: 2, username: 'wick', displayName: 'Wick', password: 'hello123' },
+    { id: 3, username: 'hulk', displayName: 'Hulk', password: 'world123' },
+    { id: 4, username: 'spiderman', displayName: 'Spiderman', password: 'hello123' },
+    { id: 5, username: 'thor', displayName: 'Thor', password: 'world123' },
+    { id: 6, username: 'batman', displayName: 'Batman', password: 'hello123' },
+    { id: 7, username: 'superman', displayName: 'Superman', password: 'world123' },
 ];
